173964c57a8975cf707eb1ca77e29caf671bef5b
Fernando Ortiz Suarez <nf.ortiz@uniandes.edu.co>
2020-08-29 12:36:18 -0500

    Link Parser: Defining the API, DFS the HTML

---
+++ link-parser/go.mod
@@ -0,0 +1,5 @@
+module link-parser
+
+go 1.13
+
+require golang.org/x/net v0.0.0-20200822124328-c89045814202
---
+++ link-parser/link/parse.go
@@ -0,0 +1,29 @@
+package link
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"io"
+)
+
+type Link struct {
+	Href string
+	Text string
+}
+
+func Parse (r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	dfs(doc, "")
+
+	return nil, nil
+}
+
+func dfs(n *html.Node, padding string) {
+	fmt.Println(padding, n.Data)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, padding+" ")
+	}
+}
