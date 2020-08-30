0ae06614bf0131a65c1de786f8b782a4da4d9789
Fernando Ortiz Suarez <nf.ortiz@uniandes.edu.co>
2020-08-30 11:18:36 -0500

    Link Parser: Building Link, Extracting node text.

--- link-parser/link/parse.go
+++ link-parser/link/parse.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"golang.org/x/net/html"
 	"io"
+	"strings"
 )
 
 type Link struct {
@@ -16,13 +17,61 @@ func Parse (r io.Reader) ([]Link, error) {
 	if err != nil {
 		return nil, err
 	}
-	dfs(doc, "")
+	nodes := linkNodes(doc)
 
-	return nil, nil
+	var links []Link
+	for _, node := range nodes {
+		links = append(links, buildLink(node))
+	}
+	return links, nil
+}
+
+func buildLink (n *html.Node) Link {
+	var ret Link
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			ret.Href = attr.Val
+			break
+		}
+	}
+
+	ret.Text = text(n)
+	return ret
+}
+
+func text(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+
+	if n.Type != html.ElementNode {
+		return ""
+	}
+
+	var ret string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret += text(c)
+	}
+	return strings.Join(strings.Fields(ret), " ")
+}
+
+func linkNodes (n *html.Node) []*html.Node {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
+	}
+	var ret []*html.Node
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret = append(ret, linkNodes(c)...)
+	}
+	return ret
 }
 
 func dfs(n *html.Node, padding string) {
-	fmt.Println(padding, n.Data)
+	msg := n.Data
+	if n.Type == html.ElementNode {
+		msg = "<" + msg + ">"
+	}
+	fmt.Println(padding, msg)
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
 		dfs(c, padding+" ")
 	}
